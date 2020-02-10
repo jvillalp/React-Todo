8093503aa917dfe4b993d08643942d98543563c6
Juana Villalpando <juana.v14@gmail.com>
2020-02-10 12:35:50 -0800

    add branch, add constructor, new todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+const todo =[
+  {
+    name:'Code App',
+    id: 123,
+    purchased: false
+  }
+];
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor()
+  addNewTodo = todoText =>{
+    const newtodo = {
+      name: todoText,
+      id: Date.now(),
+      purchased: false,
+    };
+    this.setState({
+      todoList: [ ...this.state.todoList, newtodo]
+    })
+}
+
+
   render() {
     return (
       <div>
